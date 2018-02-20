4842347102b42e036839cd92e94a161b4e2d7600
Bobarykinvl <bobarykinvl@gmail.com>
2018-02-20 15:28:06 +0300

    donegit add .!

--- src/index.js
+++ src/index.js
@@ -3,5 +3,13 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+	var trianglesCount = 0;
+		for(var i = 0; i < preferences.length; i++){
+			if(preferences[preferences[preferences[i]-1]-1]-1 == i){
+				trianglesCount++;
+			}
+		}
+	return Math.floor(trianglesCount/3);
+	
+  
 };
